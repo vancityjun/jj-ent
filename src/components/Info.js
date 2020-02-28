74807be82bfca3a6ce0e593708e5d44abd710f71
vancityjun <vancityjun@gmail.com>
2020-02-27 22:56:15 -0800

    improve design

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,5 +1,5 @@
-import React from "react";
-import Interaction from "./Interaction";
+import React from 'react'
+import Interaction from './Interaction'
 
 const Info = () => {
   return (
@@ -14,24 +14,24 @@ const Info = () => {
         Presented by&nbsp;
         <img
           className="logo"
-          src={require("../images/jjent-logo-black.svg")}
+          src={require('../images/jjent-logo-black.svg')}
           alt=""
         />
       </h3>
       <div className="detail">
         <p>Live performance by Changmo and Paul Blanco</p>
         <p>SOUND BY J.KIDO &amp; JED HARPER</p>
-        <h3>March 15th 2020</h3>
-        <h3>@ The Everleigh</h3>
+        <h3 className="date">March 15th 2020</h3>
+        <h3 className="location">@ The Everleigh</h3>
         <span>10pm - 3am</span>
         <span>580 King ST W, Toronto</span>
         <span className="miniMap">
-          <img src={require("../images/minimap.svg")} alt="" />
+          <img src={require('../images/minimap.svg')} alt="" />
         </span>
         <span className="small">Must bring your I.D +19 event</span>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Info;
+export default Info
